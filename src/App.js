19940dc26bddff943f2c0425529dd5cb6679eb9b
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-12-26 23:52:15 -0300

    criando as rotas e configurando reactotron

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import Routes from './routes';
+
+import './config/ReactotronConfig';
+
+const App = () => {
+  return <Routes />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
