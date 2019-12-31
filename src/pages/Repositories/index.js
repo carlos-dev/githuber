dd87e83e4087c0e12821bdf012a33b978acc3a9d
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-12-31 02:18:13 -0300

    fazendo requisição em Repositories

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {View, Text, AsyncStorage, ActivityIndicator} from 'react-native';
+import PropTypes from 'prop-types';
 
-import {View, Text} from 'react-native';
+import api from '../../services/api';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '../../components/header';
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositórios" />
-  </View>
-);
+import styles from './styles';
+
+export default class Repositories extends Component {
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const {data} = await api.get(`/users/${username}/repos`);
+
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({data, loading: false});
+  }
+
+  renderList = () => <Text>lista</Text>;
+
+  render() {
+    const {loading} = this.state;
+
+    return (
+      <View>
+        <Header title="Repositórios" />
+        {loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          this.renderList()
+        )}
+      </View>
+    );
+  }
+}
 
 Repositories.navigationOptions = {
   tabBarIcon: ({tintColor}) => (
     <Icon name="list-alt" size={20} color={tintColor} />
   ),
 };
-
-export default Repositories;
---
+++ src/pages/Repositories/styles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+import {metrics} from '../../styles';
+
+const styles = StyleSheet.create({
+  containe: {
+    flex: 1,
+  },
+
+  loading: {
+    marginTop: metrics.baseMargin * 2,
+  },
+});
+
+export default styles;
