fc1d2e7a725506098a4d6f4bf28b5bf764089e05
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-01-02 03:09:23 -0300

    criando a lista de organizações

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,21 +1,67 @@
-import React from 'react';
-
+import React, {Component} from 'react';
+import {View, AsyncStorage, FlatList, ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import {View, Text} from 'react-native';
-
+import api from '../../services/api';
 import Header from '../../components/header';
+import OrganizationItem from './OrganizationItem';
 
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
-);
+import styles from './styles';
+
+export default class Organizations extends Component {
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  async componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({refreshing: true});
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const {data} = await api.get(`/users/${username}/orgs`);
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({data, loading: false, refreshing: false});
+  };
+
+  renderListItem = ({item}) => <OrganizationItem organization={item} />;
+
+  renderList = () => {
+    const {data, refreshing} = this.state;
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadOrganizations}
+        refreshing={refreshing}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+      />
+    );
+  };
+
+  render() {
+    const {loading} = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+        {loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          this.renderList()
+        )}
+      </View>
+    );
+  }
+}
 
 Organizations.navigationOptions = {
   tabBarIcon: ({tintColor}) => (
     <Icon name="building" size={20} color={tintColor} />
   ),
 };
-
-export default Organizations;
